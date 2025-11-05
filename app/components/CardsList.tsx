b8b7e709c188695993ae862402884b5340cac7f5
LuthGom <lucianomendes23@yahoo.com>
2025-11-05 14:54:57 -0300

    feat(page): clening the HomePage and conteinerizing the Cards list. Add pagination

--- app/components/CardsList.tsx
+++ app/components/CardsList.tsx
@@ -1,7 +1,8 @@
 'use client';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 import ToolModal from "./ToolModal";
+import Button from "./Button";
 
 interface Tool {
     name: string;
@@ -11,27 +12,63 @@ interface Tool {
 }
 
 interface CardsListProps {
-    tools: Tool[];
+    // tools: Tool[];
+    URL: string;
 }
 
-export default function CardsList({ tools }: CardsListProps) {
+export default function CardsList({ URL }: CardsListProps) {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedTool, setSelectedTool] = useState<Tool | null>(null);
+    const [items, setItems] = useState<Tool[]>([]);
+    const [page, setPage] = useState(1);
+    const [search, setSearch] = useState("");
+    const itemsPerPage = 12;
+
+
+    const [lastClickedTools, setLastClickedTools] = useState<Tool[]>([]);
+
+    useEffect(() => {
+        fetch(URL)
+            .then((response) => response.json())
+            .then((data) => setItems(data))
+            .catch((error) => console.error("Error fetching data:", error));
+    }, [])
+
+
+    const totalPages = Math.ceil(items.length / itemsPerPage);
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const paginatedItems = items.slice(start, end);
 
     const handleToolClick = (tool: Tool) => {
         setSelectedTool(tool);
         setIsOpen(true);
+
+        setLastClickedTools((prevTools) => {
+            const updatedLastTools = [tool, ...prevTools];
+
+            setLastClickedTools([...updatedLastTools])
+            if (lastClickedTools.length > 2) {
+                lastClickedTools.pop();
+            }
+            setLastClickedTools([...updatedLastTools])
+
+
+            return prevTools
+        })
+
     };
 
     const handleCloseModal = () => {
         setIsOpen(false);
     };
 
+
     return (
         <div>
-            <div className="w-3/5 mx-auto grid grid-cols-4 grid-rows-4 gap-y-6 justify-items-center items-center min-h-screen">
-                {tools.map((tool, index) => (
-                    <Card 
+            <div className="w-4/5 mx-auto grid grid-cols-4 auto-rows-fr justify-items-center items-center h-[80vh] overflow-hidden">
+                {paginatedItems.map((tool, index) => (
+                    <Card
                         key={index}
                         name={tool.name}
                         icon={tool.icon}
@@ -40,12 +77,38 @@ export default function CardsList({ tools }: CardsListProps) {
                     />
                 ))}
             </div>
-            <ToolModal 
+            <ToolModal
                 isOpen={isOpen}
                 onClose={handleCloseModal}
                 tool={selectedTool}
                 onToolClick={handleToolClick}
+                lastedTools={lastClickedTools}
             />
+
+            {/* Controles de paginação */}
+            <div className="flex justify-center mt-4">
+                <Button
+                    onClick={() => setPage((p) => Math.max(p - 1, 1))}
+                    disabled={page === 1}
+                    name="Anterior"
+                    style="bg-[#41BAFF]"
+                >
+
+                </Button>
+
+                <span className="px-2">
+                    Página {page} de {totalPages}
+                </span>
+
+                <Button
+                    onClick={() => setPage((p) => Math.min(p + 1, totalPages))}
+                    disabled={page === totalPages}
+                    name="Próxima"
+                    style="bg-[#41BAFF]"
+                >
+
+                </Button>
+            </div>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import CardsList from "./components/CardsList";
 
-export default async function Home() {
 
-  const data = await fetch("https://pluga.co/ferramentas_search.json");
-  const tools = await data.json();
+
+export default async function Home({searchParams}: {searchParams?: {page?: string}}) {
+
+  const currentPage = Number(searchParams?.page || 1);
+  
+
+  const URL ="https://pluga.co/ferramentas_search.json";
+  // const tools = await data.json();
+
+  
+
   return (
+    
     <>
       <main className="w-4/5 mx-auto ">
 
@@ -13,7 +22,7 @@ export default async function Home() {
           <input className="w-full p-4" type="search" name="search_tool" id="search_tool" placeholder="BUSCAR FERRAMENTA" />
         </div>
 
-        <CardsList tools={tools}  ></CardsList>
+        <CardsList  URL={URL}  ></CardsList>
       </main>
     </>
   );
