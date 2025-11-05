d584153fa4f87ba676d013ca5857bc600017c0b4
LuthGom <lucianomendes23@yahoo.com>
2025-11-05 17:22:41 -0300

    refactor(components): defining ButtonProps

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,13 +1,18 @@
-'user client';
+'use client';
 
-
-export default function Button({ onClick, disabled, name, style }: { onClick: () => void; disabled: boolean, name: string,  style:string }) {
+interface ButtonProps {
+    onClick: () => void;
+    disabled?: boolean;
+    name?: string;
+    className?: string;
+}
+export default function Button({ onClick, disabled, name, className }: ButtonProps) {
 
     return (
         <button
             onClick={onClick}
             disabled={disabled}
-            className={`text-white  underline border-2 rounded-lg  py-2 px-4 hover:text-blue-800 cursor-pointer ${style}`}
+            className={`text-white  underline border-2 rounded-lg  py-2 px-4 hover:text-blue-800 cursor-pointer ${className}`}
         >
             {name}
         </button>)
